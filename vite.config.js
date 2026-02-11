1fe8a9ef0c7328009b05805e6376db190dacdcf5
Talha <ts0306706@gmail.com>
2026-02-11 16:07:54 +0500

    again try to deploy

--- vite.config.js
+++ vite.config.js
@@ -51,6 +51,9 @@ export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
   build: {
     assetsInlineLimit: 0,
+    rollupOptions: { // NEW: Add rollupOptions
+      external: ["nodemailer"], // NEW: Externalize nodemailer
+    },
   },
   optimizeDeps: {
     include: ["@shopify/app-bridge-react"],
